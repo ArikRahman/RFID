0b4e00bfe9ce5bb24a17fef4bc30fcbc56928dda
Arik Rahman <40479733+ArikRahman@users.noreply.github.com>
2025-03-30 19:52:49 -0500

    Conditional tripped incorrectly

--- sketch_mar3a/sketch_mar3a.ino
+++ sketch_mar3a/sketch_mar3a.ino
@@ -73,7 +73,7 @@ void readRFID() {
   lcd.clear();
   lcd.print("Scanning...");
   
-  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
+  if (!rfid.PICC_IsNewCardPresent() && !rfid.PICC_ReadCardSerial()) {
     Serial.println("No card detected");
     lcd.clear();
     lcd.print("No Card Found");
